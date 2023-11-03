9c06a531acd0501f98a8a4c3b3d12caf30368a6d
puck <36668@ma-web.nl>
2023-11-03 14:49:32 +0100

    edit

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ oldgreeting.greet()
 
 function basicHaiku()
 {
-    return ["Toward those short trees","We saw a batman descending","On a day in spring."]
+    return ["de neus van de haring","We sawding a batman descen","On a day in spring."]
 }
 //zet hier je haiku functie neer, zie https://github.com/progsen/haikugitopdracht voor ideeen
 
